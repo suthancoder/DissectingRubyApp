bd4d60368a66db02188e958d41ef86945c896ce1
ambasuthanjananayagam <ambasuthanjananayagam@yahoo.co.uk>
2020-05-23 20:07:02 +0100

    restarted

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :portfolios
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/index'
+  resources :blogs
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
